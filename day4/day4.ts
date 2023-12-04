77e1cda63d36a8ddeb75bd9dde7ad8ad3822c3e4
WernerLDev <7613278+WernerLDev@users.noreply.github.com>
2023-12-04 19:25:12 +0100

    Add solution for day 4

---
+++ day4/day4.ts
@@ -0,0 +1,79 @@
+import * as fs from "fs";
+
+type Card = {
+  cardId: string;
+  winningNumbers: Set<number>;
+  scratchedNumbers: Array<number>;
+};
+
+const parseLine = (line: string): Card => {
+  const values = /^Card\s+(\d+)\: (.+)\ \|\ (.+)/g.exec(line);
+
+  const winningNumbers = (values?.[2] ?? "")
+    .split(" ")
+    .map((x) => parseInt(x.trim()));
+  const scratchedNumbers = (values?.[3] ?? "")
+    .split(" ")
+    .map((x) => parseInt(x.trim()));
+
+  return {
+    cardId: values?.[1] ?? "-1",
+    winningNumbers: new Set(winningNumbers),
+    scratchedNumbers: scratchedNumbers,
+  };
+};
+
+const getWinningNumbers = (card: Card) => {
+  return card.scratchedNumbers.filter((x) => card.winningNumbers.has(x));
+};
+
+const getPointsForCard = (card: Card) => {
+  const winningNumbers = getWinningNumbers(card);
+  if (winningNumbers.length == 0) {
+    return 0;
+  }
+  return Math.pow(2, winningNumbers.length - 1);
+};
+
+const solvePart1 = (input: string[]) => {
+  return input
+    .map((line) => {
+      const card = parseLine(line);
+      return getPointsForCard(card);
+    })
+    .reduce((a, b) => a + b);
+};
+
+const getWinningCards = (card: Card, cardsMap: Map<number, Card>): number => {
+  const winningCards = getWinningNumbers(card);
+  return (
+    Array.from(Array(winningCards.length))
+      .map((_, index) => {
+        const wonCard = cardsMap.get(index + 1 + parseInt(card.cardId));
+        if (wonCard) {
+          return getWinningCards(wonCard, cardsMap);
+        }
+        return 0;
+      })
+      .reduce((a, b) => a + b, 0) + winningCards.length
+  );
+};
+
+const solvePart2 = (input: string[]) => {
+  const cards = input.map(parseLine);
+  const cardsMap = new Map(cards.map((x) => [parseInt(x.cardId), x]));
+
+  return (
+    cards
+      .map((card) => getWinningCards(card, cardsMap))
+      .reduce((a, b) => a + b) + cards.length
+  );
+};
+
+const input = fs
+  .readFileSync("day4/input.txt", "utf8")
+  .replaceAll("  ", " ")
+  .split("\n");
+console.log(solvePart1(input));
+
+console.log(solvePart2(input));
